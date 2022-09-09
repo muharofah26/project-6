b7e168e7f5d0c12183a9a93249f43220bfc59d3e
Luqinul <110762217+muharofah26@users.noreply.github.com>
2022-09-09 16:52:22 +0700

    Initial commit

---
+++ System/resources/views/produk/show.blade.php
@@ -0,0 +1,27 @@
+@extends('admin.base')
+
+@section ('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-15">
+    <div class="card">
+        <div class="card-header">
+            Detail Data Produk
+</div>
+<div class="card-body">
+    <h3>{{$produk->nama}}</h3>
+    <hr>
+    <p>
+        Rp. {{number_format($produk->harga)}}
+        stok : {{$produk->stok}}
+    </p>
+    <p>
+        {{$produk->deskripsi}}
+</p>
+ </div>
+ </div>
+ </div>
+</div>
+</div>
+@endsection
